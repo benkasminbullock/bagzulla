09153fc3ce0251476aaaee2f57517a492111a1c8
Ben Bullock <benkasminbullock@gmail.com>
2023-04-28 09:34:15 +0900

    Indent init.pl; use /usr/bin/env to run it

--- scripts/init.pl
+++ scripts/init.pl
@@ -1,9 +1,11 @@
-#!/home/ben/software/install/bin/perl
+#!/usr/bin/env perl
 use warnings;
 use strict;
 use FindBin '$Bin';
+
 use JSON::Parse 'read_json';
 use DBI;
+
 chdir "$Bin/.." or die $!;
 unlink 'bagzulla.db' or die $!;
 system ("sqlite3 -batch bagzulla.db < schema.txt");
@@ -11,9 +13,9 @@ my $db = DBI->connect('dbi:SQLite:dbname=bagzulla.db','','',
 		      {RaiseError => 1, AutoCommit => 1});
 my $users = read_json ("users.json");
 for my $user (@$users) {
-my $p = $user->{pass};
-my $n = $user->{login};
-$db->do ("INSERT INTO person(name,email,password) VALUES('$n','$n\@localhost','$p')");
+    my $p = $user->{pass};
+    my $n = $user->{login};
+    $db->do ("INSERT INTO person(name,email,password) VALUES('$n','$n\@localhost','$p')");
 }
 $db->do ("INSERT INTO txt(content) VALUES('Project unspecified')");
 $db->do ("INSERT INTO project(name,directory,description,owner,status) VALUES('None','',1,1,0)");
